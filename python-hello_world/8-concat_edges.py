6c10b07d3ec6a62d81bf0045ffc23826af2d36dd
stephanetchatchum <stephanetchatchum@gmail.com>
2026-02-22 08:33:49 +0200

    restore correct 8-concat_edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-str3 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str3[39:67] + str3[107:112] + str3[0:6])
+str3 = "Python School, object-oriented programming with Python!"
+print(str3[15:48] + str3[0:6])
