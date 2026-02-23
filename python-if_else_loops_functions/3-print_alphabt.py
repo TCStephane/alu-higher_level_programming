d03a5ccd3eb369259603c7cbc35e0a91faf2fd08
stephanetchatchum <stephanetchatchum@gmail.com>
2026-02-23 22:56:45 +0200

    Fix execute permission

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(["{:c}".format(i) for i in range(ord('a'), ord('d') +1)]), end="")
+print("".join(["{:c}".format(i) for i in range(ord('a'), ord('z') +1 ) if i != ord('e') and i != ord('q')]), end="")
