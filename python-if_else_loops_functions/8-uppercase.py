feb1bac32909fec263edc99ad033bb3a91450308
stephanetchatchum <stephanetchatchum@gmail.com>
2026-02-24 09:47:27 +0200

    Fix execute permission

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,7 @@ def uppercase(str):
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
             result += chr(ord(char) - 32)
-            return result
         else:
             result += char
-            return result
+            
     print("{}".format(result))
