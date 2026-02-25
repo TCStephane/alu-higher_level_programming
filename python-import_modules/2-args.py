793a46ae1a0e9dfa79588ff8f7aa3ba4355d0ed3
stephanetchatchum <stephanetchatchum@gmail.com>
2026-02-25 15:23:47 +0200

    Hello

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arguments = sys.args[1:]
+    count = len(arguments)
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 arguments.")
+    else:
+        print(f"{count} arguments:")
+    for i in range(count):
+        print(f"{i + 1}: {arguments[i]}")
+    
