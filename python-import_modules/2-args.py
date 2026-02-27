c0c6258bad2428275f7c218ef2e68e6e061b0b12
TCStephane <t.stephane@alustudent.com>
2026-02-27 10:00:55 +0200

    Fix argument handling and improve output formatting

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+
 import sys
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
+    arguments = sys.args[1:]
     count = len(arguments)
 
     if count == 0:
         print("0 arguments.")
-    # elif count == 1:
-    #     print("1 arguments:")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print(f"{count} arguments:")
     for i in range(count):
-        print(f"{i+1}: {arguments[i]}")
-        # .format(i+1, arguments[i])
+        print(f"{i + 1}: {arguments[i]}")
